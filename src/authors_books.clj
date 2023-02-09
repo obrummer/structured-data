bf09c7915b95b5cafb5ad79773d42c63c868ef30
Olli Brummer <olli.brummer@knowit.fi>
2023-02-09 08:26:30 +0200

    answers

---
+++ src/authors_books.clj
@@ -0,0 +1,28 @@
+(ns authors-books)
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def friedman {:name "Daniel Friedman" :birth-year 1944})
+(def felleisen {:name "Matthias Felleisen"})
+
+(def cities {:title "The City and the City" :authors [china]})
+(def wild-seed {:title "Wild Seed", :authors [octavia]})
+(def embassytown {:title "Embassytown", :authors [china]})
+(def little-schemer {:title "The Little Schemer"
+                     :authors [friedman, felleisen]})
+(def books [cities, wild-seed, embassytown, little-schemer])
+(def authorss #{china, felleisen, octavia, friedman})
+
+(def jrrtolkien {:name "J. R. R. Tolkien" :birth-year 1892 :death-year 1973})
+(def christopher {:name "Christopher Tolkien" :birth-year 1924})
+(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
+
+(def silmarillion {:title "Silmarillion"
+                   :authors #{jrrtolkien, christopher, kay}})
+
+(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
+
+(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,23 @@
-(ns structured-data)
+(ns structured-data
+  (:require [authors-books :refer :all]))
 
 (defn do-a-thing [x]
-  :-)
+  (let [xx (+ x x)]
+  (Math/pow xx xx)))
 
 (defn spiff [v]
-  :-)
+  (let [first (get v 0)
+        third (get v 2)]
+  (+ first third)))
 
 (defn cutify [v]
-  :-)
+  (let [item "<3"]
+(conj v item)
+))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let [[first second third] v]
+  (+ first third)))
 
 (defn point [x y]
   [x y])
@@ -19,96 +26,155 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+  (- x2 x1)))
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+  (- y2 y1)))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+  (== (- x2 x1)(- y2 y1))))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+  (* (- x2 x1)(- y2 y1))))
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle 
+        [x y] point]
+  (and (<= x1 x x2) (<= y1 y y2))))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[[x1 y1] [x2 y2]] outer
+        [[x3 y3] [x4 y4]] inner]
+  (and (contains-point? (rectangle [x1 y1] [x2 y2]) (point x3 y3))
+       (contains-point? (rectangle [x1 y1] [x2 y2]) (point x4 y4)))       
+))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
+
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (not (== (count (:authors book)) 1)))
 
 (defn add-author [book new-author]
-  :-)
+  (let [authors (:authors book)] 
+    (assoc book :authors (conj authors new-author))))
 
 (defn alive? [author]
-  :-)
+  (let [year (:death-year author)] 
+    (if year false true))
+  )
+
+(defn get-length [element]
+  (count element)
+  )
 
 (defn element-lengths [collection]
-  :-)
+  (map get-length collection))
 
 (defn second-elements [collection]
-  :-)
+   (let [second-element (fn [x] (get x 1))]
+    (map second-element collection)))
 
 (defn titles [books]
-  :-)
+  (map :title books))
 
 (defn monotonic? [a-seq]
-  :-)
+  (or (apply <= a-seq) (apply >= a-seq)))
+
 
 (defn stars [n]
-  :-)
+   (let [amount (repeat n "*")] 
+     (apply str amount)))
+
 
 (defn toggle [a-set elem]
-  :-)
+  (if (contains? a-set elem) (disj a-set elem) (conj a-set elem)))
+
 
 (defn contains-duplicates? [a-seq]
-  :-)
+  (not (== (count a-seq) (count (set a-seq)))))
 
 (defn old-book->new-book [book]
-  :-)
+  (let [authors (:authors book)]
+  (assoc book :authors (set authors))))
 
 (defn has-author? [book author]
-  :-)
+  (let [new-book (old-book->new-book book)]
+  (contains? (:authors new-book) author)))
+
+(defn get-authors [book]
+  (let [authors (:authors book)]
+  (set authors)))
+
+(defn get-author-names [book]
+  (let [authors (:authors book)]
+  (set authors)))
+
 
 (defn authors [books]
-  :-)
+    (apply clojure.set/union (map get-authors books)))
+
 
 (defn all-author-names [books]
-  :-)
+  (set (map :name (authors books))))
 
 (defn author->string [author]
-  :-)
+  (let [author-name (:name author)
+        birth-year (:birth-year author)
+        death-year (:death-year author)]
+    (if (> (count (str birth-year)) 0) 
+      (str author-name " (" birth-year " - " death-year ")") 
+      (str author-name))))
+
 
 (defn authors->string [authors]
-  :-)
+  (apply str (interpose ", " (set (map author->string authors)))))
 
 (defn book->string [book]
-  :-)
+  (let [book-title (:title book)
+        book-authors (:authors book)]
+    (str book-title ", written by " (authors->string book-authors))))
 
 (defn books->string [books]
-  :-)
+  (let [book-amount (count books)] 
+   (if (== book-amount 0) 
+    (str "No books.") 
+    (str (str book-amount) 
+         (if (== book-amount 1) (str " book. ") (str " books. ")) 
+         (apply str (interpose ". " (set (map book->string books))))
+         "."))))
 
 (defn books-by-author [author books]
-  :-)
+  (filter (fn [x] (has-author? x author)) books))
 
 (defn author-by-name [name authors]
-  :-)
+  (let [authors-by-name 
+        (apply clojure.set/union (filter (fn [x] (= name (:name x))) authors))]
+    (if (== (count authors-by-name) 0) 
+      nil 
+      (apply clojure.set/union (filter (fn [x] (= name (:name x))) authors)))
+  ))
 
 (defn living-authors [authors]
-  :-)
+  (filter (fn [x] (alive? x)) authors))
+
 
 (defn has-a-living-author? [book]
-  :-)
+  (if (empty? (living-authors (:authors book))) false true))
+
+(has-a-living-author? wild-seed)
+
+
 
 (defn books-by-living-authors [books]
-  :-)
+  (filter (fn [x] (has-a-living-author? x)) books))
 
 ; %________%
